4c9187cc3633900f09cea39f960b090faa604fac
Matteo Sali <salimatteo@gmail.com>
2021-01-29 21:05:34 +0100

    Added Character with equip functions (not tested)

--- Assets/02-Scripts/Utility.cs
+++ Assets/02-Scripts/Utility.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace DefaultNamespace {
 
 	public static class Utility {
@@ -14,6 +16,19 @@ namespace DefaultNamespace {
 
 		public static int CalculateItemBattleValue(Item item) {
 			return (int) ((item.MaxHp + item.Attack + item.Defense) * RarityLevelMultiplier(item.RarityLevel));
+		}
+
+		public static int EquipableTypeIndex(CharacterClass characterClass, ItemType type) {
+			for (int i = 0; i < characterClass.EquipableTypes.Length; i++) {
+				if (characterClass.EquipableTypes[i] == type) {
+					return i;
+				}
+			}
+			
+			Debug.LogError("Item Type not found.\n" +
+			               $"Class: {characterClass.Name}\n" +
+			               $"ItemType: {type.Name}");
+			return -1;
 		}
 	}
 
