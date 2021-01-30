2cb9dd92b84a0e0f9fed980e735c971d65f00ba5
Alessio Ebani <ebani.alessio@studenti-ittfedifermi.edu.it>
2021-01-30 17:21:43 +0100

    Telecamera bellina

--- Assets/02-Scripts/Follow_Player.cs
+++ Assets/02-Scripts/Follow_Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Follow_Player : MonoBehaviour
@@ -8,13 +9,52 @@ public class Follow_Player : MonoBehaviour
     public Transform target;
     public float zoom;
 
+    public List<MeshRenderer> disabledObjects=new List<MeshRenderer>();
+    
     void Update() {
         Vector3 targetpos=Vector3.zero;
         Vector3 dir = transform.forward;
         dir.Normalize();
         targetpos = target.position+(-dir * zoom);
-        
         // Move the camera smoothly to the target position
         transform.position = targetpos;
+        
+        RaycastHit[] hits;
+        // you can also use CapsuleCastAll()
+        // TODO: setup your layermask it improve performance and filter your hits.
+        hits = Physics.BoxCastAll(transform.position,Vector3.one, target.position-transform.position,Quaternion.identity, Vector3.Distance(transform.position,target.position));
+        foreach(RaycastHit hit in hits)
+        {
+            MeshRenderer R = hit.collider.GetComponent<MeshRenderer>();
+            if (R == null)
+                continue; // no renderer attached? go to next hit
+            // TODO: maybe implement here a check for GOs that should not be affected like the player
+            if (!disabledObjects.Contains(R)) {
+                disabledObjects.Add(R);
+            }
+        }
+        foreach (MeshRenderer render in disabledObjects) {
+            if (ContainsMeshRenderer(hits, render)) {
+                render.enabled = false;
+            }
+            else {
+                render.enabled = true;
+            }
+        }
+        
+    }
+
+    public bool ContainsMeshRenderer(RaycastHit[] hits, MeshRenderer renderer) {
+        foreach(RaycastHit hit in hits)
+        {
+            MeshRenderer R = hit.collider.GetComponent<MeshRenderer>();
+            if (renderer == R) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos() {
     }
 }
