e200fbb270d36116a9404d08ab33c07e91456307
Alessio Ebani <ebani.alessio@studenti-ittfedifermi.edu.it>
2021-01-30 20:05:45 +0100

    Audio

---
+++ Assets/02-Scripts/Sounds/SimpleAudioEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAudioEvent : AudioEvent
+{
+    public List<AudioClip> clips;
+    public float volume, pitch; //min, max and other stuff...
+    public override void Play(AudioSource audio)
+    {	
+        audio.volume = volume;
+        audio.pitch = pitch;
+        audio.clip = clips[Random.Range(0, clips.Count)];
+        audio.Play();
+    }
+}
+
